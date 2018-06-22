caf9545d7cda1beee359685fb46ff3891f884e7d
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-22 19:09:12 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.backer = self unless project.backer == self
+    project.add_backer(self) unless project.backers.include?(self)
   end
 
 
